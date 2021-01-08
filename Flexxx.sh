5fc32c855718f57f9a7c30322e63e340bd4c5c2f
Coordinate-Cat <tetrapasta02@gmail.com>
2021-01-08 22:59:06 +0900

    general

--- Flexxx.sh
+++ Flexxx.sh
@@ -4,140 +4,90 @@ _SENTAKU_SEPARATOR=$'\n'
 _SENTAKU_NOHEADER=1
 _SENTAKU_NONUMBER=1
 
-# main menu
+# SNS LIST
 menu="
-  1: SNS
-  2: test
-  3: test
-  4: test
-  5: test
+   Github    : (https://github.com)
+   Twitter   : (https://twitter.com)
+   Instagram : (https://www.instagram.com)
+   Reddit    : (https://www.reddit.com)
+   Facebook  : (https://www.facebook.com)
+   Pinterest : (https://jp.pinterest.com)
+   Telegram  : (https://web.telegram.org)
+   Keybase   : (https://keybase.io)
+  ﭮ Discord   : (https://discord.com)
+   5ch       : (https://5ch.net)
 "
 
-_sf_initialize_user () {
-  push=""
-}
+# SNS URL PUSH
+_sf_1 () { open https://twitter.com/      }
+_sf_2 () { open https://www.instagram.com }
+_sf_3 () { open https://www.facebook.com  }
+_sf_4 () { open https://www.reddit.com    }
+_sf_5 () { open https://jp.pinterest.com  }
+_sf_6 () { open https://web.telegram.org  }
+_sf_7 () { open https://keybase.io        }
+_sf_8 () { open https://github.com        }
+_sf_9 () { open https://5ch.net           }
 
-_sf_execute () {
-  echo "$push"
-}
-
-# First layer
-_sf_1 () {
-  push=$(_sf_links)
-  case "$push" in
-    *Twitter*)
-      open https://twitter.com/
-      exit
-    ;;
-    *Instagram*)
-      open https://www.instagram.com
-      exit
-    ;;
-    *Facebook*)
-      open https://www.facebook.com
-      exit
-    ;;
-    *Reddit*)
-      open https://www.reddit.com
-      exit
-    ;;
-    *Pinterest*)
-      open https://jp.pinterest.com
-      exit
-    ;;
-    *Telegram*)
-      open https://web.telegram.org
-      exit
-    ;;
-    *Keybase*)
-      open https://keybase.io
-      exit
-    ;;
-    *Github*)
-      open https://github.com
-      exit
-    ;;
-  esac
-}
-_sf_2 () {
-  echo Under maintenance
-}
-_sf_3 () {
-  echo Under maintenance
-}
-_sf_4 () {
-  echo Under maintenance
-}
-_sf_5 () {
-  echo Under maintenance
-}
-
-# First layer
 func (){
-  _sf_1 () { # SNS
+  _sf_1 () {
     _s_current_n=0
-    open https://twitter.com/
-      exit
     _s_break=1
   }
-  _sf_2 () { # test
+  _sf_2 () {
     _s_current_n=1
-    open https://www.instagram.com
-    exit
     _s_break=1
   }
-  _sf_3 () { # test
+  _sf_3 () {
     _s_current_n=2
     _s_break=1
   }
-  _sf_4 () { # test
+  _sf_4 () {
     _s_current_n=3
     _s_break=1
   }
-  _sf_5 () { # test
+  _sf_5 () {
     _s_current_n=4
     _s_break=1
   }
+  _sf_6 () {
+    _s_current_n=5
+    _s_break=1
+  }
+  _sf_7 () {
+    _s_current_n=6
+    _s_break=1
+  }
+  _sf_8 () {
+    _s_current_n=7
+    _s_break=1
+  }
+  _sf_9 () {
+    _s_current_n=8
+    _s_break=1
+  }
 }
-# First layer
+
 _sf_select () {
-  if   [ $_s_current_n -eq 0 ];then # SNS
+  if   [ $_s_current_n -eq 0 ];then
     _sf_1
-  elif [ $_s_current_n -eq 1 ];then # test
+  elif [ $_s_current_n -eq 1 ];then
     _sf_2
-  elif [ $_s_current_n -eq 2 ];then # test
+  elif [ $_s_current_n -eq 2 ];then
     _sf_3
-  elif [ $_s_current_n -eq 3 ];then # test
+  elif [ $_s_current_n -eq 3 ];then
     _sf_4
-  elif [ $_s_current_n -eq 4 ];then # test
+  elif [ $_s_current_n -eq 4 ];then
     _sf_5
+  elif [ $_s_current_n -eq 5 ];then
+    _sf_6
+  elif [ $_s_current_n -eq 6 ];then
+    _sf_7
+  elif [ $_s_current_n -eq 7 ];then
+    _sf_8
+  elif [ $_s_current_n -eq 8 ];then
+    _sf_9
   fi
 }
-
-
-_sf_links () { # {{
-. sentaku -n -c
-_SENTAKU_SEPARATOR=$'\n'
-_SENTAKU_NOHEADER=1
-_SENTAKU_NONUMBER=1
-
-menu="
-  1 |  Twitter   : (https://twitter.com/)
-  2 |  Instagram : (https://www.instagram.com)
-  3 |  Facebook  : (https://www.facebook.com)
-  4 |  Reddit    : (https://www.reddit.com)
-  5 |  Pinterest : (https://jp.pinterest.com)
-  6 |  Telegram  : (https://web.telegram.org)
-  7 |  Keybase   : (https://keybase.io)
-  8 |  Github    : (https://github.com)
-"
-
-_sf_execute () {
-  echo ${_s_inputs[$_s_current_n]: 7}
-}
-
-func
-echo "$menu" | _sf_main
-} # }}
 
 echo "$menu" | _sf_main "$@"
---
+++ README.md
@@ -0,0 +1,12 @@
+# Flexxx
+## 汎用エイリアス一覧展開高速移動シェルスクリプト(仮
+
+## lists
+| list name | file name  |
+| :-------- | :--------- |
+| sns       | [sns.sh]() |
+
+
+
+### Special Thanks
+[rcmdnk](https://github.com/rcmdnk)/[sentaku](https://github.com/rcmdnk/sentaku)
